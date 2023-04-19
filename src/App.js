1441667c2ac6de3355ed1e57f57a4ff65ed412b9
ankitsharma1506 <ankitsharma1506.work@gmail.com>
2023-04-19 18:08:16 +0530

    demo changes for aquib to check

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Navbar from './components/navbar';
 import BasicDetails from './components/basicDetails/basicDetails.js';
 import CourseDetails from './components/courseDetails/courseDetails.js';
 import RelocationDetail from './components/relocationDetails/relocationDetails.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      {/* <Login /> */}
-      <BasicDetails />
-      {/* <CourseDetails /> */}
-      {/* <RelocationDetail /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path = '/' element = { <Login /> } />
+          <Route path = '/basicDetails' element = { <BasicDetails /> } />
+          <Route path = '/courseDetails' element = { <CourseDetails /> } />
+          <Route path = '/relocationDetails' element = { <RelocationDetail /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/relocationDetails/relocationDetails.js
+++ src/components/relocationDetails/relocationDetails.js
@@ -6,11 +6,58 @@ function RelocationDetail() {
   return (
     <div className='box'>
         <div>
-            <span>Willing to go outside district for employmment?</span>
-            <Form.Group name="relocation">
-                <Form.Check className="yes-no" type="radio" label="Yes" name="relocation" value={1} />
-                <Form.Check className="yes-no" type="radio" label="No" name="relocation" value={0} />
-            </Form.Group>
+            <div className="row">
+              <div className="col-sm-6">
+                <span>Willing to go outside district for employment?</span>
+              </div>
+              <div className = "col-sm-6">
+                <Form.Group name="relocation">
+                  <div className='row'>
+                    <div className='col-sm-6'>
+                      <Form.Check className="yes-no1" type="radio" label="Yes" name="relocation" value={1} />
+                    </div>
+                    <div className='col-sm-6'>
+                      <Form.Check className="yes-no1" type="radio" label="No" name="relocation" value={0} />
+                    </div>
+                  </div>
+                </Form.Group>
+              </div>
+            </div>
+            {(() => {
+              if (yes-no.value) {<div className="row">
+              <div className="col-sm-6">
+                <span>Willing to go outside State for employment?</span>
+              </div>
+              <div className = "col-sm-6">
+                <Form.Group name="relocation">
+                  <div className='row'>
+                    <div className='col-sm-6'>
+                      <Form.Check className="yes-no2" type="radio" label="Yes" name="relocation" value={1} />
+                    </div>
+                    <div className='col-sm-6'>
+                      <Form.Check className="yes-no2" type="radio" label="No" name="relocation" value={0} />
+                    </div>
+                  </div>
+                </Form.Group>
+              </div>
+            </div>}})()}
+            <div className="row">
+              <div className="col-sm-6">
+                <span>Willing to go outside India for employment?</span>
+              </div>
+              <div className = "col-sm-6">
+                <Form.Group name="relocation">
+                  <div className='row'>
+                    <div className='col-sm-6'>
+                      <Form.Check className="yes-no3" type="radio" label="Yes" name="relocation" value={1} />
+                    </div>
+                    <div className='col-sm-6'>
+                      <Form.Check className="yes-no3" type="radio" label="No" name="relocation" value={0} />
+                    </div>
+                  </div>
+                </Form.Group>
+              </div>
+            </div>
         </div>
     </div>
   )
